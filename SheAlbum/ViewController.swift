678e513d85b46d549c65e86a8a87303f19a9c18b
yida1207 <f263370@yahoo.com.tw>
2020-08-08 15:33:10 +0800

    Initial Commit

---
+++ SheAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SheAlbum
+//
+//  Created by Yida on 2020/8/8.
+//  Copyright © 2020 Yida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
